80c529ac36e6a93c9d48ff0fa6dc598e2127ff74
crazydudo <crazydudo>
2019-09-23 19:06:20 +0800

    loading animation

--- app/src/main/java/com/cd/igitandroid/ui/test/TestActivity.java
+++ app/src/main/java/com/cd/igitandroid/ui/test/TestActivity.java
@@ -2,15 +2,22 @@ package com.cd.igitandroid.ui.test;
 
 import android.os.Bundle;
 
+import com.airbnb.lottie.LottieAnimationView;
 import com.cd.igitandroid.R;
 
 import androidx.appcompat.app.AppCompatActivity;
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class TestActivity extends AppCompatActivity {
+
+    @BindView(R.id.animation_view)
+    LottieAnimationView animationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test);
+        ButterKnife.bind(this);
     }
 }
